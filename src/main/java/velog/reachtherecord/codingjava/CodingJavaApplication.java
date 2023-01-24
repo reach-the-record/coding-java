8770bc18a487be4bb718d22cc85be37fc37b4fea
Chanseong <reachtherecord@gmail.com>
2023-01-24 18:10:26 +0900

    first commit

---
+++ src/main/java/velog/reachtherecord/codingjava/CodingJavaApplication.java
@@ -0,0 +1,13 @@
+package velog.reachtherecord.codingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/velog/reachtherecord/codingjava/CodingJavaApplicationTests.java
@@ -0,0 +1,11 @@
+package velog.reachtherecord.codingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingJavaApplicationTests {
+
+
+
+}
